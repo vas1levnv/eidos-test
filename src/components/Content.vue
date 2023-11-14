d78e7c5e7d010e066ce1a6c12e35ea83b653e49c
vas1levnv <vas1levnv@yandex.ru>
2023-11-15 00:13:28 +0300

    Пагинация

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,36 +1,46 @@
-<script setup>
-
+<script>
 import ContentList from "@/components/ContentList.vue";
 import SortBtnFirst from "@/components/icons/SortBtnFirst.vue";
 import SortBtnSecond from "@/components/icons/SortBtnSecond.vue";
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  components: {SortBtnSecond, SortBtnFirst, ContentList},
+  data() {
+    return {
+      searchQuery: ''
+    }
+  }
+})
+
 </script>
 
 <template>
-	<div class="content">
-		<div class="content-wrapper">
-			<div class="content-header">
-				<div class="content-header__title">
-					Учебные сессий
-				</div>
-				<input class="content-header__search" type="text">
-				<SortBtnFirst/>
-				<SortBtnSecond/>
-			</div>
-			<ContentList/>
-		</div>
-	</div>
+  <div class="content">
+    <div class="content-wrapper">
+      <div class="content-header">
+        <div class="content-header__title">
+          Учебные сессий {{ searchQuery }}
+        </div>
+        <input class="content-header__search" v-model="searchQuery" type="text">
+        <SortBtnFirst/>
+        <SortBtnSecond/>
+      </div>
+      <ContentList :searchQuery="searchQuery"/>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .content {
-	height: 100%;
-	padding: 0.5rem;
-	
-	&-wrapper {
-		height: 100%;
-		background: white;
-		border-radius: 0.75rem;
-		padding: 1.5rem;
-	}
+  height: 100%;
+  padding: 0.5rem;
+
+  &-wrapper {
+    height: 100%;
+    background: white;
+    border-radius: 0.75rem;
+    padding: 1.5rem;
+  }
 }
 </style>
--- src/components/ContentList.vue
+++ src/components/ContentList.vue
@@ -1,13 +1,126 @@
-<script setup>
+<script>
+import axios from "axios";
 
-const name = 'asfasf'
+export default {
+  props: {
+    searchQuery: String
+  },
+  data() {
+    return {
+      items: [],
+      sortTitle: '',
+      page: 1,
+      limit: 10,
+      totalPages: 0,
+    }
+  },
+
+  mounted() {
+    this.fetchDataFromApi()
+  },
+
+  methods: {
+
+    async fetchDataFromApi() {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10', {
+          params: {
+            _page: this.page,
+            _limit: this.limit,
+          }
+        })
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
+        this.items = response.data
+      } catch (e) {
+        console.log(e)
+      }
+    },
+    changePage(pageNumber) {
+      this.page = pageNumber
+      this.fetchDataFromApi()
+    },
+    changeDecrementPage() {
+      if (this.page !== 1) {
+        this.page -= 1
+        this.fetchDataFromApi()
+      }
+    },
+
+    changeIncrementPage() {
+      if (this.page !== this.totalPages) {
+        this.page += 1
+        this.fetchDataFromApi()
+      }
+    },
+
+    changeSortTitle(e) {
+      this.sortTitle = e.target.innerText
+    }
+  },
+  computed: {
+    sortedList() {
+      return [...this.items].sort((post1, post2) => post1[this.sortTitle]?.localeCompare(post2[this.sortTitle]))
+    },
+    sortedAndSearchedList() {
+      return this.sortedList.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
+    },
+  },
+}
+
 
 </script>
 
 <template>
-{{ name}}
+  <div class="content-list">
+    <table>
+      <thead>
+      <tr>
+        <th @click="changeSortTitle">title</th>
+        <th @click="changeSortTitle">body</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="item in sortedAndSearchedList" :key="item.id">
+        <td>{{ item.title }}</td>
+        <td>{{ item.body }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+  <div class="pagination">
+    <div @click="changeDecrementPage">prev</div>
+    <div v-for="pageNumber in totalPages" :key="pageNumber" @click="changePage(pageNumber)">
+      <div class="pagination-item"
+           :class="{selected: page === pageNumber}"
+           v-show="pageNumber === 1 ||
+            pageNumber === totalPages ||
+             pageNumber === page ||
+              pageNumber === page + 1||
+               pageNumber === page - 1 ||
+               pageNumber === page + 2 ||
+               pageNumber === page - 2
+"
+      >{{ pageNumber }}
+      </div>
+    </div>
+    <div @click="changeIncrementPage">next</div>
+  </div>
 </template>
 
 <style scoped lang="scss">
 
+.content-list {
+  margin-top: 2rem;
+}
+
+.pagination {
+  display: flex;
+  align-items: center;
+
+  &-item {
+    &.selected {
+      background: teal;
+    }
+  }
+}
 </style>
